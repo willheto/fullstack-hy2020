46766fe5186533ae1c788e504d72c7559e08963d
Henri Willman <60850956+willheto@users.noreply.github.com>
2020-04-04 20:52:47 +0300

    1.7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,26 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (
-    <div>
-      <p><h1>give feedback</h1></p>
-      <Button text="good" onClick={() => setGood(good + 1)} />
-      <Button text="neutral" onClick={() => setNeutral(neutral + 1)}/>
-      <Button text="bad" onClick={() => setBad(bad + 1)}/>
-      <p><h1>statistics</h1></p>
-      <Result number={good} name="good" />
-      <Result number={neutral} name="neutral"/>
-      <Result number={bad} name="bad"/>
-    </div>
-  )
-}
-
 const Button = (props) => {
   return (
     <button onClick={props.onClick}>
@@ -37,6 +17,60 @@ const Result = (props) => {
   )
 }
 
-ReactDOM.render(<App />, 
+const Statistics = (props) => {
+  const good = props.allFeedback[0]
+  const neutral = props.allFeedback[1]
+  const bad = props.allFeedback[2]
+
+  const endResult = (good + bad * -1) / (good + neutral + bad)
+  const endP = good / (neutral + bad + good) * 100
+
+  if (!isNaN(endResult)) {
+    return (
+      <div>
+        <div>
+          average {endResult}
+        </div>
+        <div>
+          positive {endP} %
+        </div>
+      </div>
+    )
+  } return (
+    <div>
+      <div>
+        average 0
+      </div>
+      <div>
+        positive 0 %
+      </div>
+    </div>
+  )
+
+
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      <Result number={good} name="good" />
+      <Result number={neutral} name="neutral" />
+      <Result number={bad} name="bad" />
+      <Statistics allFeedback={[good, neutral, bad]} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
